3962ecd7a1219fc6c135ea564657e11f5aef66ea
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2024-09-13 16:58:14 +0200

    add petri net for readers and writers

---
+++ src/main/scala/u06/examples/PNReadersAndWriters.scala
@@ -0,0 +1,26 @@
+package scala.u06.examples
+
+export u06.modelling.PetriNet
+import u06.utils.MSet
+
+object PNReadersAndWriters:
+
+  enum Place:
+    case p1, p2, p3, p4, p5, p6, p7
+
+  export Place.*
+  export u06.modelling.SystemAnalysis.*
+  export u06.modelling.PetriNet.*
+
+  def pnRW = PetriNet[Place](
+    MSet(p1) ~~> MSet(p2),
+    MSet(p2) ~~> MSet(p3),
+    MSet(p2) ~~> MSet(p4),
+    MSet(p3, p5) ~~> MSet(p5, p6),
+    MSet(p4, p5) ~~> MSet(p7) ^^^ MSet(p6),
+    MSet(p6) ~~> MSet(p1),
+    MSet(p7) ~~> MSet(p5, p1),
+  ).toSystem
+
+  given initialMark: MSet[Place] = MSet(p1, p1, p1, p1)
+  given depth: Int = 100
