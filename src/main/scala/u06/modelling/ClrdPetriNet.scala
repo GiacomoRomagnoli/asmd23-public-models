7b925d71577e1e40fb9c20186d9220abcfb55477
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2024-09-26 16:25:37 +0200

    colored petri net effect coloring (first attempt)

--- src/main/scala/u06/modelling/ClrdPetriNet.scala
+++ src/main/scala/u06/modelling/ClrdPetriNet.scala
@@ -4,11 +4,10 @@ import u06.modelling.System
 import u06.utils.MSet
 
 object ClrdPetriNet:
+  case class Trn[P, C](cond: MSet[P], eff: MSet[(P, C)] => MSet[(P, C)], inh: MSet[P])
+  type PetriNet[P, C] = Set[Trn[P, C]]
 
-  export PetriNet.{Trn, Marking, PetriNet}
-  type Token[P, C] = (P, C)
-
-  extension [P, C](self: MSet[Token[P, C]])
+  extension [P, C](self: MSet[(P, C)])
     private def color(cond: MSet[P]) =
       val groupByPlaces = self.asList.groupBy((p, _) => p)
       for
@@ -18,15 +17,27 @@ object ClrdPetriNet:
         c <- tokens.combinations(nc)
       yield MSet.ofList(c)
 
-  extension [P, C](pn: PetriNet[P])
-    def toSystem: System[Marking[Token[P, C]]] = mc =>
+  extension [P, C](pn: PetriNet[P, C])
+    def toSystem: System[MSet[(P, C)]] = mc =>
       val m = MSet.ofList(mc.asList.map((p, _) => p))
-      val transitions = for
+      for
         trn <- pn
-        if m disjoined trn.inh
+        if m `disjoined` trn.inh
         cond <- mc.color(trn.cond)
-        // gli effetti non si colorano allo stesso modo riguardaci!
-        eff <- cond.color(trn.eff)
-        out <- mc extract cond
-      yield out union eff
-      Set()
+        out <- mc `extract` cond
+      yield out `union` trn.eff(cond)
+
+  extension [P, C](cond: MSet[P])
+    def ~~>> (eff: MSet[(P, C)]): Trn[P, C] =  Trn(cond, _ => eff, MSet())
+    def ~~> (eff: MSet[(P, C)] => MSet[(P, C)]): Trn[P, C] = Trn(cond, eff, MSet())
+    def ~~> (eff: MSet[P]): Trn[P, C] = if cond.size != eff.size then
+      throw IllegalArgumentException()
+    else
+      Trn(
+        cond,
+        condition => MSet.ofList((for i <- 0 to condition.size yield (eff.asList(i), condition.asList(i)._2)).toList),
+        MSet()
+      )
+
+  extension [P, C](trn: Trn[P, C])
+    def ^^^ (z: MSet[P]): Trn[P, C] = trn.copy(inh = z)
